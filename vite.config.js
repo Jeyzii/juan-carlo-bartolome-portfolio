3b9d7a887f364b326dc71ada2dbd872da9b52a6e
Lok <89766268+Jeyzii@users.noreply.github.com>
2026-01-20 14:02:51 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/juan-carlo-bartolome-portfolio",
+  base: "/juan-carlo-bartolome-portfolio/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
